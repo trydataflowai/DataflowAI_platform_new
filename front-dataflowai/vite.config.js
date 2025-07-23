9f51f3a6f18eac2b512d9053327d0c0d51165507
herrenojuliancoltrade <herrenojulian@coltrade.com.co>
2025-07-23 18:31:02 -0500

    Fix Vite routing for Render

--- front-dataflowai/vite.config.js
+++ front-dataflowai/vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Carga automáticamente las variables del archivo .env o .env.production
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return {
     plugins: [react()],
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+      historyApiFallback: true, // Esto es importante solo en local
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
   }
 })
